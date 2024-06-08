1301b845133a11e6bd1afc6086cc31d8996c7e5d
nyawox <nyawox.git@gmail.com>
2024-06-08 17:41:36 +0900

    add sequences

--- kanata.nix
+++ kanata.nix
@@ -21,9 +21,9 @@ with lib; let
     (deflayer psilocybin
       XX   XX    XX   XX   XX   XX   XX    XX   XX   XX   XX   XX   XX   XX   XX
       XX   @x    @l   @c   @m   @k   XX    XX   @z   @f   @u   @o   @y   @.
-      @lrp @n    @r   @s   @t   @g   XX    XX   @b   @mgt @e   @a   @i
+      @⬢   @n    @r   @s   @t   @g   XX    XX   @b   @⚝   @e   @a   @i
       XX   @j    @w   @p   @d   @q   XX    XX   @v   @h   @;   @'
-      XX   @lmet               @spc            @ral @rmet @ral
+      XX   @lmet               @spc            @ral @rmet @⬡
     )
 
     (deflayer psilocybin-tp
@@ -77,9 +77,9 @@ with lib; let
     (deflayer psilocybin
       XX   XX    XX   XX   XX   XX   XX    XX   XX   XX   XX   XX   XX   XX  XX
       XX   @x    @l   @c   @m   @k   XX    XX   @z   @f   @u   @o   @y   @.
-      @lrp @n    @r   @s   @t   @g   XX    XX   @b   @mgt @e   @a   @i
+      @⬢   @n    @r   @s   @t   @g   XX    XX   @b   @⚝   @e   @a   @i
       XX   @j    @w   @p   @d   @q   XX    XX   @v   @h   @;   @'   @'
-      XX   XX    lalt @lmet     @spc       XX    XX  XX   @rmet @ral
+      XX   XX    lalt @lmet     @spc       XX    XX  XX   @rmet @⬡
     )
 
     (deflayer psilocybin-tp
@@ -126,151 +126,6 @@ with lib; let
     ;; typing layer are used to disable chord while typing fast
     (deffakekeys
       to-base (layer-switch psilocybin)
-      the (one-shot 5000 (layer-toggle the))
-      the-r (one-shot 5000 (layer-toggle the-r))
-      the-r-e (one-shot 5000 (layer-toggle the-r-e))
-      the-r-o (one-shot 5000 (layer-toggle the-r-o))
-      tho (one-shot 5000 (layer-toggle tho))
-      they (one-shot 5000 (layer-toggle they))
-      for (one-shot 5000 (layer-toggle for))
-      for-w (one-shot 5000 (layer-toggle for-w))
-      for-n (one-shot 5000 (layer-toggle for-n))
-      for-r (one-shot 5000 (layer-toggle for-r))
-      be (one-shot 5000 (layer-toggle be))
-      be-c (one-shot 5000 (layer-toggle be-c))
-      be-f (one-shot 5000 (layer-toggle be-f))
-      be-u (one-shot 5000 (layer-toggle be-u))
-      beauty (one-shot 5000 (layer-toggle beauty))
-      beautifully (one-shot 5000 (layer-toggle beautifully))
-      be-t (one-shot 5000 (layer-toggle be-t))
-      be-g (one-shot 5000 (layer-toggle be-g))
-      wh (one-shot 5000 (layer-toggle wh))
-      will (one-shot 5000 (layer-toggle will))
-      when (one-shot 5000 (layer-toggle when))
-      what (one-shot 5000 (layer-toggle what))
-      whats (one-shot 5000 (layer-toggle whats))
-    )
-
-    ;; these word-builder rules aren't tested and likely wouldn't work on jis keyboard
-    ;; <spc>*
-    (deflayermap (the)
-      spc (macro e spc) ;; <spc>*<spc> -> the<spc>
-      ralt (macro e m) ;; <spc>*<rpt> -> them
-      ] (macro e y) ;; <spc>*y -> they
-      a (macro e n) ;; <spc>*n -> then
-      d (macro e s e) ;; <spc>*s -> these
-      s (macro r (on-press tap-vkey the-r)) ;; <spc>*r -> the**(including r)
-      [ (macro o (on-press tap-vkey tho)) ;; <spc>*o -> tho**
-    )
-    (deflayermap (the-r)
-      spc (macro bspc e i r spc) ;; <spc>*r<spc> -> their<spc>
-      ; (macro e (on-press tap-vkey the-r-e)) ;; <spc>*re
-      [ (macro o (on-press tap-vkey the-r-o)) ;; <spc>*ro
-    )
-    (deflayermap (the-r-e)
-      spc (macro bspc bspc e r e spc) ;; <spc>*re<spc> -> there
-    )
-    (deflayermap (the-r-o)
-      spc (macro u g h spc) ;; <spc>*ro<spc> -> through
-    )
-    (deflayermap (tho)
-      d (macro s e) ;; <spc>*os  -> those
-      . (macro u g h) ;; <spc>*oh -> though
-    )
-    (deflayermap (they)
-      s (macro r e) ;; <spc>*y<rpt>r -> they're
-      , (macro v e) ;; <spc>*y<rpt>v -> they've ;; this is the 4th hardest word in this layout
-      w (macro l l) ;; <spc>*y<rpt>l -> they'll
-    )
-    ;; f<rpt>
-    (deflayermap (for) ;; defaults to for
-      v (macro bspc u n d) ;; f<rpt>d -> found
-      g (macro e i g n) ;; f<rpt>g -> foreign
-      x (on-press tap-vkey for-w) ;; f<rpt>w
-      a (on-press tap-vkey for-n) ;; f<rpt>n
-      s (on-press tap-vkey for-r) ;; f<rpt>r
-    )
-    ;; prevent confliction with forward
-    (deflayermap (for-w)
-      spc (macro bspc l l o w spc) ;; f<rpt>w<spc> -> follow
-    )
-    ;; prevent confliction with california
-    (deflayermap (for-n)
-      spc (macro g o t t e n spc) ;; f<rpt>n<spc> -> forgotten
-    )
-    (deflayermap (for-r)
-      spc (macro e v e r spc) ;; f<rpt>r<spc> -> forever
-      v (macro w a r d) ;; f<rpt>rd -> forward
-    )
-    ;; <spc><rpt>
-    (deflayermap (be) ;; defaults to be
-      t (macro bspc a c k) ;; <spc><rpt>k -> back
-      e (macro c (on-press tap-vkey be-c)) ;; <spc><rpt>c
-      o (macro f (on-press tap-vkey be-f)) ;; <spc><rpt>f
-      p (macro u (on-press tap-vkey be-u)) ;; <spc><rpt>u
-      f (macro t (on-press tap-vkey be-t)) ;; <spc><rpt>t
-      g (macro g (on-press tap-vkey be-g)) ;; <spc><rpt>g
-    )
-    (deflayermap (be-c)
-      spc (macro a u s e spc) ;; <spc><rpt>c<spc> -> because
-      [ (macro o m e) ;; <spc><rpt>co -> become
-      ' (macro a m e) ;; <spc><rpt>co -> became
-      ret (macro o m i n g) ;; <spc><rpt>ci -> becoming
-    )
-    (deflayermap (be-f)
-      spc (macro o r e spc) ;; <spc><rpt>f -> before
-    )
-    (deflayermap (be-u)
-      spc (macro bspc bspc u t spc) ;; <spc><rpt>u<spc> -> but
-      f (macro bspc a u t y (on-press tap-vkey beauty)) ;; <spc><rpt>ut -> beauty
-    )
-    (deflayermap (beauty)
-      o (macro bspc i f u l (on-press tap-vkey beautifully)) ;; <spc><rpt>utf -> beautiful
-      ] (macro bspc i f y) ;; <spc><rpt>uty -> beautify
-    )
-    (deflayermap (beautifully)
-      ] (macro l y) ;; <spc><rpt>utfy -> beautifully
-    )
-    (deflayermap (be-t)
-      a (macro w e e n) ;; <spc><rpt>tn -> between
-      ; (macro t e r) ;; <spc><rpt>te -> better
-      s (macro r a y) ;; <spc><rpt>tr -> betray
-    )
-    (deflayermap (be-g)
-      ret (macro i n) ;; <spc><rpt>gi -> begin
-      ' (macro a n) ;; <spc><rpt>ga -> began
-      p (macro u n) ;; <spc><rpt>gu -> begun
-    )
-    ;; <spc><lrpt>
-    (deflayermap (wh)
-      l (macro bspc i t h) ;; <spc><lrpt>* -> with
-      ralt (macro e n (on-press tap-vkey when)) ;; <spc><lrpt><rpt> -> when
-      [ (macro o) ;; <spc><lrpt>o -> who
-      ] (macro y) ;; <spc><lrpt>y -> why
-      ' (macro a t (on-press tap-vkey what)) ;; <spc><lrpt>a -> what
-      ret (macro bspc i l l (on-press tap-vkey will)) ;; <spc><lrpt>i -> will
-      w (macro bspc e l l) ;; <spc><lrpt>l -> well
-      . (macro i l e) ;; <spc><lrpt>h -> while
-      e (macro i c h) ;; <spc><lrpt>c -> which
-      ; (macro e r e) ;; <spc><lrpt>e -> where
-      t (macro bspc o r k) ;; <spc><lrpt>k -> work
-      p (macro bspc o u l d) ;; <spc><lrpt>u -> would
-      v (macro bspc o r l d) ;; <spc><lrpt>d -> world
-      f (macro e t h e r) ;; <spc><lrpt>t -> whether
-    )
-    (deflayermap (will)
-      f (macro bspc bspc t h o u t) ;; <spc><lrpt>it -> without
-    )
-    (deflayermap (when)
-      , (macro e v e r) ;; <spc><lrpt><rpt>v -> whenever
-    )
-    (deflayermap (what)
-      , (macro e v e r) ;; <spc><lrpt>av -> whatever
-      d (macro s (on-press tap-vkey whats)) ;; <spc><lrpt>as
-    )
-    (deflayermap (whats)
-      spc (macro bspc ' s spc) ;; <spc><lrpt>as<spc> -> what's
-      , (macro o e v e r) ;; <spc><lrpt>asv -> whatsoever
     )
 
     (defalias
@@ -292,122 +147,6 @@ with lib; let
       ctl (one-shot 2000 lctl)
       alt (one-shot 2000 lalt)
       cmd (one-shot 2000 lmet)
-
-
-      magic (switch ;;🪄
-        ;;;;;;;;;;;;;;;;;;;;;;;;
-        ;; psilocybin
-        ((key-history m 1)) p break ;; sfb
-        ((key-history y 1)) . break ;; sfb
-        ((and (key-history w 2) (key-history a 1))) s break ;; was ;; wao doesn't happen in english
-        ((and (key-history w 2) (key-history i 1))) s break ;; wis ;; wion doesn't happen in english
-        ((and (key-history w 2) (key-history e 1))) s break ;; wes ;; weu doesn't happen in english
-        ((and (key-history m 4) (key-history a 3) (key-history k 2) (key-history e 1))) u break ;; prevent overwriting makeup
-        ((and (key-history k 2) (key-history e 1))) d break ;; ked
-        ((and (key-history a 5) (key-history m 4) (key-history a 3) (key-history t 2) (key-history e 1))) u break ;; te* -> ted unless amateur
-        ((and (key-history l 3) (key-history m 2) (key-history a 1))) o break ;; stop converting lmao to lmat
-        ((and (key-history m 2) (key-history a 1))) t break ;; ma* -> mat sfs
-        ((and (key-history m 2) (key-history e 1))) t break ;; ma* -> met sfs
-        ((and (key-history t 2) (key-history e 1))) d break ;; te* -> ted sfs
-        ((and (key-history g 2) (key-history e 1))) d break ;; ge* -> ged sfs
-        ((and (key-history d 2) (key-history a 1))) t break ;; da* -> dat sfs
-        ((key-history e 1)) u break ;; sfb
-        ;; most pms which is sfb are pment, use left rpt key for other pms
-        ((and (key-history l 3)(key-history o 2) (key-history p 1))) (macro m e n t) break ;; opment
-        ((and (key-history o 3)(key-history o 2) (key-history p 1))) (macro m e n t) break ;; opment
-        ;; remove script from the rule below
-        ((and (key-history s 5)(key-history c 4)(key-history r 3) (key-history i 2) (key-history p 1))) t break
-        ((and (key-history i 2) (key-history p 1))) (macro m e n t) break ;; ipment
-        ((key-history p 1)) t break ;; sfb
-        ((key-history r 1)) l break ;; sfb
-        ((key-history w 1)) s break ;; sfb
-        ((and (key-history g 3) (key-history a 2) (key-history d 1))) g break ;; gadget
-        ((and (key-history b 3) (key-history a 2) (key-history d 1))) g break ;; badge
-        ((and (key-history a 2) (key-history d 1))) m break ;; dm sfb mostly consists of adm, otherwise cope with index middle
-        ((key-history d 1)) g break ;; sfb
-        ((key-history s 1)) w break ;; sfb
-        ((key-history l 1)) r break ;; sfb
-        ((key-history a 1)) o break ;; sfb
-        ;;;;;;;;;;;;;;;
-        ;;th is the most common english bigram
-        ((or (key-history spc 1) (key-history tab 1) (key-history ret 1))) (macro t h (on-press tap-vkey the)) break
-        ;;tment most common tm words are tment
-        ;; Put delay to avoid macro being interrupted when the key isn't released. key-release didn't work
-        ((and (key-history r 2) (key-history t 1))) (macro m e n 50 t) break ;; rtment
-        ((and (key-history n 2) (key-history t 1))) (macro m e n 50 t) break ;; ntment
-        ((and (key-history s 2) (key-history t 1))) (macro m e n 50 t) break ;; stment
-        ((and (key-history f 2) (key-history t 1))) (macro m e n 50 t) break ;; ftment
-        ((and (key-history e 3) (key-history a 2) (key-history t 1))) (macro m e n 50 t) break ;; eatment
-        ((and (key-history u 3) (key-history i 2) (key-history t 1))) (macro m e n 50 t) break ;; uitment
-        ((and (key-history m 3) (key-history i 2) (key-history t 1))) (macro m e n 50 t) break ;; mitment
-        ;;ion
-        ((key-history i 1)) (macro o n) break
-
-        ;;tch
-        ((key-history t 1)) (macro c h) break
-        ;; ver
-        ((key-history v 1)) (macro e r) break ;; press with index middle, otherwise it just makes another sfb
-        ;; just, most frequent ngram that starts with j
-        ((key-history j 1)) (macro u s t) break
-
-        ;; ght is a common sfs
-        ((key-history g 1)) (macro h t) break
-        ;; got sfs
-        ((and (key-history g 2) (key-history o 1))) t break
-        ;; nts common redirect
-        ((key-history n 1)) (macro t s) break
-        ;; put sfs
-        ((and (key-history c 5) (key-history a 4) (key-history m 3) (key-history p 2) (key-history u 1))) (macro s e) break ;; campuses prevent overwriting use with put
-        ((and (key-history r 8) (key-history t 7) (key-history o 6) (key-history c 5) (key-history a 4) (key-history r 3) (key-history p 2) (key-history u 1))) (macro s e) break ;; artocarpuses prevent overwriting use with put
-        ((and (key-history p 2) (key-history u 1))) t break
-        ;; top
-        ((and (key-history t 2) (key-history o 1))) p break
-        ;; stud
-        ((and (key-history s 3) (key-history t 2) (key-history u 1))) d break
-        ;; use
-        ((key-history u 1)) (macro s e) break
-
-        ;; TODO somehow implement ious
-      )
-      mgc (chord esc *)
-      mgt (multi @mgc @tp)
-
-      repeat (switch
-        ;; Use this thumb repeat key mostly when repeating left hand bigram like ll
-        ;; they'
-        ((and (key-history t 4) (key-history h 3) (key-history e 2) (key-history y 1))) (macro ' (on-press tap-vkey they)) break
-        ;; ing
-        ((key-history i 1)) (macro n g) break
-        ((key-history y 1)) (macro i n g) break ;; sfb all common yis are ying
-        ((and (key-history n 2) (key-history g 1))) (macro i n 50 g) break ;; nging
-        ((and (key-history n 2) (key-history d 1))) (macro i n g) break ;; nding
-        ((and (key-history o 2) (key-history w 1))) (macro i n g) break ;; owing
-        ((and (key-history r 3) (key-history a 2) (key-history w 1))) (macro i n g) break ;; rawing
-        ((and (key-history r 3) (key-history o 2) (key-history w 1))) (macro i n g) break ;; rowing
-        ((and (key-history e 2) (key-history w 1))) (macro i n g) break ;; ewing
-        ((key-history v 1)) (macro i n g) break ;; ving
-        ((key-history h 1)) (macro a v) break ;; sfs
-        ((key-history a 1)) (macro n d) break ;; common ngram
-        ((key-history u 1)) (macro r e) break ;; ure
-        ;; for
-        ((key-history f 1)) (macro o r (on-press tap-vkey for)) break
-        ;; be
-        ((or (key-history spc 1) (key-history tab 1) (key-history ret 1))) (macro b e (on-press tap-vkey be)) break
-        () rpt break
-      )
-
-      ral @repeat
-
-      lrp (switch
-        ((and (key-history i 2) (key-history p 1))) (macro t) break
-        ((key-history p 1)) m break ;; sfb, use right repeat key for pp, magic key for pment
-        ((key-history k 1)) (macro i n g) break ;; the rule is to put on the opposite hand repeat key
-        ((key-history t 1)) p break ;; sfb
-        ((key-history g 1)) (macro e t) break ;; get sfs
-        ;; wh
-        ((or (key-history spc 1) (key-history tab 1) (key-history ret 1))) (macro w h (on-press tap-vkey wh)) break
-        () rpt break
-      )
 
       spc (tap-hold 200 300 spc lctl)
       lmet (chord thumb lm)
@@ -480,12 +219,6 @@ with lib; let
       (lm rm ) @num
     )
 
-    (defchords esc 25
-      (*    ) @magic
-      (   f ) f
-      (*  f ) esc
-    )
-
     (defchords backspace 25
       (h   ) h
       (  ; ) ;
@@ -496,7 +229,9 @@ with lib; let
       hngl 122 ;;ime on/off
     )
   '';
+  cfg = config.psilocybin;
 in {
+  imports = [./magic.nix];
   options = {
     psilocybin = {
       enable = mkOption {
@@ -513,6 +248,9 @@ in {
           An empty list, the default value, lets kanata detect which input devices are keyboards and intercept them all.
         '';
       };
+      magic = mkOption {
+        type = types.str;
+      };
       ansi = mkOption {
         type = types.bool;
         default =
@@ -527,18 +265,18 @@ in {
     };
   };
   config = {
-    services.kanata = mkIf config.psilocybin.enable {
+    services.kanata = mkIf cfg.enable {
       enable = true;
 
-      keyboards.psilocybin = mkIf config.psilocybin.ansi {
+      keyboards.psilocybin = mkIf cfg.ansi {
         extraDefCfg = defCfg;
-        config = ansi + psilocybin;
-        devices = config.psilocybin.devices;
+        config = ansi + psilocybin + cfg.magic;
+        devices = cfg.devices;
       };
-      keyboards.psilocybinjis = mkIf config.psilocybin.jis {
+      keyboards.psilocybinjis = mkIf cfg.jis {
         extraDefCfg = defCfg;
-        config = jis + psilocybin;
-        devices = config.psilocybin.devices;
+        config = jis + psilocybin + cfg.magic;
+        devices = cfg.devices;
       };
     };
   };
---
+++ magic.nix
@@ -0,0 +1,166 @@
+{lib, ...}: {
+  config = {
+    # ⚝ -> nop0
+    # ⬡ -> nop1
+    # ⬢ -> nop2
+    psilocybin.magic = lib.mkDefault ''
+      (defcfg
+        sequence-timeout 2000
+        sequence-input-mode visible-backspaced
+        sequence-always-on true
+      )
+      (defalias
+        mgc (chord esc *)
+        ⚝ (multi (chord esc ⚝) @tp)
+        ⬡ nop1
+        ⬢ nop2
+      )
+      (defchords esc 25
+        (⚝    ) nop0
+        (   f ) f
+        (⚝  f ) esc
+      )
+      (deftemplate seq (vk-name input-keys output-action)
+            (deffakekeys $vk-name $output-action)
+            (defseq $vk-name $input-keys)
+      )
+      ;;⚝
+      (t! seq mp (m nop0) (macro m (unmod p))) ;; sfb
+      (t! seq y. (y nop0) (macro y (unmod .))) ;; sfb
+      (t! seq was (w a nop0) (macro w (unmod a s))) ;; wao doesn't happen
+      (t! seq wis (w i nop0) (macro w (unmod i s))) ;; wion doesn't happen
+      (t! seq wes (w e nop0) (macro w (unmod e s))) ;; weu doesn't happen
+      (t! seq makeup (m a k e nop0) (macro m (unmod a k e u))) ;; prevent ked overriding makeup
+      (t! seq ked (k e nop0) (macro k (unmod e d))) ;; sfs
+      (t! seq amateur (a m a t e nop0) (macro a (unmod m a t e u))) ;; prevent ted overriding amateur
+      (t! seq lmao (l m a nop0) (macro l (unmod m a o))) ;; prevent mat overriding lmao
+      (t! seq mat (m a nop0) (macro m (unmod a t))) ;; sfs
+      (t! seq met (m e nop0) (macro m (unmod e t))) ;; sfs
+      (t! seq ted (t e nop0) (macro t (unmod e d))) ;; sfs
+      (t! seq ged (g e nop0) (macro g (unmod e d))) ;; sfs
+      (t! seq dat (d a nop0) (macro d (unmod a t))) ;; sfs
+      (t! seq eu (e nop0) (macro e (unmod u))) ;; sfb
+      (t! seq lopment (l o p nop0) (macro l (unmod o p m e n t))) ;; opment
+      (t! seq oopment (o o p nop0) (macro o (unmod o p m e n t))) ;; opment
+      (t! seq script (s c r i p nop0) (macro s (unmod c r i p t))) ;; prevent ipment overriding script
+      (t! seq oopment (i p nop0) (macro i (unmod p m e n t))) ;; ipment
+      (t! seq pt (p nop0) (macro p (unmod t))) ;; sfb
+      (t! seq rl (r nop0) (macro r (unmod l))) ;; sfb
+      (t! seq ws (w nop0) (macro w (unmod s))) ;; sfb
+      (t! seq gadget (g a d nop0) (macro g (unmod a d g))) ;; prevent adm overriding gadget
+      (t! seq badge (b a d nop0) (macro b (unmod a d g))) ;; prevent adm overriding badge
+      (t! seq adm (a d nop0) (macro a (unmod d m))) ;; most of dm sfb
+      (t! seq dg (d nop0) (macro d (unmod g))) ;; sfb
+      (t! seq sw (s nop0) (macro s (unmod w))) ;; sfb
+      (t! seq lr (l nop0) (macro l (unmod r))) ;; sfb
+      (t! seq ao (a nop0) (macro a (unmod o))) ;; sfb
+      ;; th
+      (t! seq the (spc nop0 spc) (macro t (unmod h e spc)))
+      (t! seq them (spc nop0 nop1) (macro t (unmod h e m)))
+      ;; they, I will keep in this order to see if order matters in sequences
+      (t! seq they (spc nop0 y) (macro t (unmod h e y)))
+      (t! seq they're (spc nop0 y nop1 r) (macro t (unmod h e y ' r e)))
+      (t! seq they've (spc nop0 y nop1 v) (macro t (unmod h e y ' v e)))
+      (t! seq they'll (spc nop0 y nop1 l) (macro t (unmod h e y ' l l)))
+      (t! seq then (spc nop0 n) (macro t (unmod h e n)))
+      (t! seq these (spc nop0 s) (macro t (unmod h e s e)))
+      (t! seq their (spc nop0 r spc) (macro t (unmod h e i r spc)))
+      (t! seq there (spc nop0 r e spc) (macro t (unmod h e r e spc)))
+      (t! seq through (spc nop0 r o spc) (macro t (unmod h r o u g h spc)))
+      (t! seq those (spc nop0 o s) (macro t (unmod h o s e)))
+      (t! seq though (spc nop0 o h) (macro t (unmod h o u g h)))
+      (t! seq th (spc nop0) (macro t (unmod h)))
+      ;; most common tm words are tment
+      ;; put delay to avoid macro being interrupted when the key isn't released yet.
+      (t! seq rtment (r t nop0) (macro r (unmod t m e n 50 t))) ;; rtment
+      (t! seq ntment (n t nop0) (macro n (unmod t m e n 50 t))) ;; ntment
+      (t! seq stment (s t nop0) (macro s (unmod t m e n 50 t))) ;; stment
+      (t! seq ftment (f t nop0) (macro f (unmod t m e n 50 t))) ;; ftment
+      (t! seq eatment (e a t nop0) (macro e (unmod a t m e n t))) ;; eatment
+      (t! seq uitment (u i t nop0) (macro u (unmod i t m e n t))) ;; uitment
+      (t! seq mitment (m i t nop0) (macro m (unmod i t m e n t))) ;; mitment
+
+      (t! seq ion (i nop0) (macro i (unmod o n))) ;; ion
+      (t! seq tch (t nop0) (macro t (unmod c h))) ;; tch
+      (t! seq ver (v nop0) (macro v (unmod e r))) ;; ver
+      (t! seq just (j nop0) (macro j (unmod u s t))) ;; just
+      (t! seq ght (g nop0) (macro g (unmod h t))) ;; ght sfs
+      (t! seq got (g o nop0) (macro g (unmod o t))) ;; got sfs
+      (t! seq nts (n nop0) (macro n (unmod t s))) ;; nts sfs
+      ;; put sfs
+      (t! seq campuse (c a m p u nop0) (macro c (unmod a m p u s e))) ;; prevent put overriding campuses
+      (t! seq artocarpuses (a r t o c a r p u nop0) (macro a (unmod r t o c a r p u s e))) ;; prevent put overriding artocarpuses
+      (t! seq put (p u nop0) (macro p (unmod u t)))
+
+      (t! seq top (t o nop0) (macro t (unmod o p))) ;; sfs
+      (t! seq stud (s t u d nop0) (macro s (unmod t u d))) ;; most of the tud sfs
+      (t! seq use (u s nop0) (macro u (unmod s e))) ;; sfs
+      ;;⬡
+      (t! seq ing (i nop1) (macro i (unmod n g)))
+      (t! seq ying (y nop1) (macro y (unmod i n g)))
+      (t! seq nging (n g nop1) (macro n (unmod g i n 50 g)))
+      (t! seq nding (n d nop1) (macro n (unmod d i n g)))
+      (t! seq owing (o w nop1) (macro o (unmod w i n g)))
+      (t! seq rawing (r a w nop1) (macro r (unmod a w i n g)))
+      (t! seq ewing (e w nop1) (macro e (unmod w i n g)))
+      (t! seq ving (v nop1) (macro v (unmod i n g)))
+      (t! seq hav (h nop1) (macro h (unmod a v))) ;; sfs
+      (t! seq and (a nop1) (macro a (unmod n d)))
+      (t! seq ure (u nop1) (macro u (unmod r e)))
+      ;; for
+      (t! seq found (f nop1 d) (macro f (unmod o u n d)))
+      (t! seq foreign (f nop1 g) (macro f (unmod o r e i g n)))
+      (t! seq follow (f nop1 w spc) (macro f (unmod o l l o w spc))) ;; spc is necessary to prevent confliction with forward
+      (t! seq forgotten (f nop1 n spc) (macro f (unmod o r g o t t e n spc))) ;; spc is necessary to prevent confliction with california
+      (t! seq forever (f nop1 r spc) (macro f (unmod o u n d)))
+      (t! seq forward (f nop1 r d) (macro f (unmod o r w a r d)))
+      (t! seq for (f nop1) (macro f (unmod o r)))
+      ;; be
+      (t! seq because (spc nop1 c spc) (macro b (unmod e c a u s e spc)))
+      (t! seq become (spc nop1 c o) (macro b (unmod e c o m e)))
+      (t! seq became (spc nop1 c a) (macro b (unmod e c a m e)))
+      (t! seq becoming (spc nop1 c i) (macro b (unmod e c o m i n g)))
+      (t! seq before (spc nop1 f spc) (macro b (unmod e f o r e spc)))
+      (t! seq but (spc nop1 u spc) (macro b (unmod u t spc)))
+      (t! seq beauty (spc nop1 u t) (macro b (unmod e a u t y)))
+      (t! seq beautiful (spc nop1 u t f) (macro b (unmod e a u t i f u l)))
+      (t! seq beautifully (spc nop1 u t f y) (macro b (unmod e a u t i f u l l y)))
+      (t! seq beautify (spc nop1 u t y) (macro b (unmod e a u t i f y)))
+      (t! seq between (spc nop1 t n) (macro b (unmod e t w e e n)))
+      (t! seq better (spc nop1 t e) (macro b (unmod e t t e r)))
+      (t! seq betray (spc nop1 t r) (macro b (unmod e t r a y)))
+      (t! seq begin (spc nop1 g i) (macro b (unmod e g i n)))
+      (t! seq began (spc nop1 g a) (macro b (unmod e g a n)))
+      (t! seq begun (spc nop1 g u) (macro b (unmod e g u n)))
+      (t! seq back (spc nop1 k) (macro b (unmod a c k)))
+      (t! seq be (spc nop1) (macro b (unmod e)))
+      ;;⬢
+      (t! seq ipt (i p nop2) (macro i (unmod p t)))
+      (t! seq pm (p nop2) (macro p (unmod m))) ;; sfb
+      (t! seq tp (t nop2) (macro t (unmod p))) ;; sfb
+      (t! seq king (k nop2) (macro k (unmod i n g)))
+      (t! seq get (g nop2) (macro g (unmod e t))) ;; sfs
+      ;; wh
+      (t! seq with (spc nop2 nop0) (macro w (unmod i t h)))
+      (t! seq whenever (spc nop2 nop1 v) (macro w (unmod h e n e v e r)))
+      (t! seq when (spc nop2 nop1) (macro w (unmod h e n)))
+      (t! seq who (spc nop2 o) (macro w (unmod h o)))
+      (t! seq why (spc nop2 y) (macro w (unmod h y)))
+      (t! seq whatever (spc nop2 a v) (macro w (unmod h a t e v e r)))
+      (t! seq whats (spc nop2 a s spc) (macro w (unmod h a t ' s spc)))
+      (t! seq whatsoever (spc nop2 a s v) (macro w (unmod h a t s o e v e r)))
+      (t! seq what (spc nop2 a) (macro w (unmod h a t)))
+      (t! seq without (spc nop2 i t) (macro w (unmod i t h o u t)))
+      (t! seq will (spc nop2 i) (macro w (unmod i l l)))
+      (t! seq well (spc nop2 l) (macro w (unmod e l l)))
+      (t! seq while (spc nop2 h) (macro w (unmod h i l e)))
+      (t! seq which (spc nop2 c) (macro w (unmod h i c h)))
+      (t! seq where (spc nop2 e) (macro w (unmod h e r e)))
+      (t! seq would (spc nop2 u) (macro w (unmod o u l d)))
+      (t! seq world (spc nop2 d) (macro w (unmod o r l d)))
+      (t! seq whether (spc nop2 t) (macro w (unmod h e t h e r)))
+      (t! seq wh (spc nop2) (macro w (unmod h))) ;; defaults to wh
+
+    '';
+  };
+}
